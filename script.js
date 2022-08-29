e1778a29e5b5d8f124aadc7ae8ae20e6d852af66
Sharayah Hefner <rocknrayah@gmail.com>
2022-08-28 20:57:53 -0600

    thank you page

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//---------------------BUTTON FORMAT ------------------
 const form = document.querySelector("form"),
         nextBtn = form.querySelector(".nextBtn"),
         backBtn = form.querySelector(".backBtn"),
@@ -14,7 +16,10 @@ nextBtn.addEventListener("click", ()=> {
     })
 })
 
-backBtn.addEventListener("click", () => form.classList.remove('secActive'));
+backBtn.addEventListener("click",() => form.classList.remove('secActive'));
+
+
+// ---------------Phone Number Format ------------------
 
 function formatPhoneNumber(value) {
   if (!value) return value;
@@ -31,4 +36,56 @@ function phoneNumberFormatter() {
   const inputField = document.getElementById('phone-number');
   const formattedInputValue = formatPhoneNumber(inputField.value);
   inputField.value = formattedInputValue;
+}
+
+
+// -----------Social Security Format----------
+
+function formatSocialNumber(value) {
+  if (!value) return value;
+  const socialNumber = value.replace(/[^\d]/g,'');
+  const socialNumberLength = socialNumber.length;
+  if (socialNumberLength < 4) return socialNumber;
+  if(socialNumberLength < 7) {
+    return `${socialNumber.slice(0,3)}-${socialNumber.slice(3)}`;
+  }
+  return `${socialNumber.slice(0,3)}-${socialNumber.slice(3,5,)}-${socialNumber.slice(5,8)}`;
+}
+
+function socialNumberFormatter() {
+  const inputField = document.getElementById('social-number');
+  const formattedInputValue2 = formatSocialNumber(inputField.value);
+  inputField.value = formattedInputValue2;
+}
+
+
+
+// ----------------State Formatter----------------------------
+
+
+const inputE1 = docutment.querySelector("#autocomplete-input");
+inputE1.addEventListener("submit",onInputChange);
+
+getStateData();
+let stateNames = [];
+
+ function getStateData() {
+  
+  const states = [
+{  name: 'Alaska'},
+{  name: 'Texas'},
+{  name: 'Alabama'},
+{  name: 'Arkansas'},
+{  name: 'Arizona'},
+{  name: 'California'},
+{ name: 'Colorado'},
+  ];
+  return states.name;
+ 
+
+}
+console.log(getStateData)
+
+function onInputChange() {
+console.log(inputE1.value)
 }
